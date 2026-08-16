ce59f9246769d83f2d75f57f3c1609aa074574c9
Tinah024 <kaberacristine@gmail.com>
2026-08-16 17:44:55 +0300

    Prepare project for deployment

--- siwes_system/settings.py
+++ siwes_system/settings.py
@@ -25,9 +25,9 @@ load_dotenv(BASE_DIR / '.env')
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 
 # Application definition
